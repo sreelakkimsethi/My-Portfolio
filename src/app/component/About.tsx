ea5cb093e25dd0c011258319bbbb1000c2119b81
sravya5454 <sravyamogali16@gmail.com>
2025-03-02 16:12:13 +0530

    checking updating or not

--- src/app/component/About.tsx
+++ src/app/component/About.tsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
                 <div className="home">
                 <p className="description">
     Hello! I&apos;m <span className="name-highlight">Sravya Mogali</span>, I am a motivated and detail-oriented web developer with expertise in React.js, Next.js, JavaScript, HTML, and CSS. I thrive in building responsive and dynamic web applications, integrating REST APIs, and managing state with Redux Toolkit. My passion lies in crafting user-centric solutions while embracing new technologies. With hands-on experience in full-stack development and cloud storage security, I bring a problem-solving mindset to Agile-driven teams. I am eager to contribute my skills to innovative projects and continuously grow as a developer.
-    <br /> I have showcased my technical skills by presenting papers at IIT Hyderabad and leading &quot;TechSphere 2023,&quot; a successful college-wide technical event. My internship experiences as a JavaScript and Full Stack Developer have strengthened my ability to build robust and scalable applications. I am excited to collaborate on impactful projects that push the boundaries of web development.
+    <br /> I have showcased my technical skills by presenting papers at IIT Hyderabad and leading &quot;TechSphere 2023,&quot; a successful college-wide technical event. My internship experiences as a JavaScript and Full Stack Developer have strengthened my ability to build robust and scalable applications. I am excited to collaborate on impactful projects that push the boundaries of web development..
 </p>
 
                 </div>
