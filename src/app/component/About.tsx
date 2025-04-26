4e0195538c8da0c502f1be29c7046f888a188f6e
Nandavamsi07 <nandavamsisingireddy@gmail.com>
2025-04-26 16:03:15 +0530

    second_commit

--- src/app/component/About.tsx
+++ src/app/component/About.tsx
@@ -8,11 +8,11 @@ const AboutMe = () => {
             <div className="content-section">
                 <div className="home">
                     <p className="description">
-                        Hello! I'm <span className="name-highlight">DEVISRI Lakkimsetti</span>, a passionate <strong>Full Stack Developer</strong> skilled in <strong>React.js, Node.js, JavaScript, HTML, CSS, and Bootstrap</strong>. I build responsive, user-focused web applications and seamlessly integrate REST APIs.
+                        Hello! I&apos;m <span className="name-highlight">DEVISRI Lakkimsetti</span>, a passionate <strong>Full Stack Developer</strong> skilled in <strong>React.js, Node.js, JavaScript, HTML, CSS, and Bootstrap</strong>. I build responsive, user-focused web applications and seamlessly integrate REST APIs.
                         <br /><br />
-                        With experience in <strong>Full-Stack Development</strong> and <strong>AI/ML</strong>, I bring a strong problem-solving mindset to Agile teams. I've contributed to real-time, scalable projects during my internships.
+                        With experience in <strong>Full-Stack Development</strong> and <strong>AI/ML</strong>, I bring a strong problem-solving mindset to Agile teams. I&apos;ve contributed to real-time, scalable projects during my internships.
                         <br /><br />
-                        I'm a quick learner who thrives in collaborative environments and enjoys tackling complex challenges.
+                        I&apos;m a quick learner who thrives in collaborative environments and enjoys tackling complex challenges.
                         <br /><br />
                         Outside of coding, I love exploring new tech, participating in hackathons, and working on impactful, AI-driven web solutions.
                         <br /><br />
